ee8ed4dbb9f130495d6894f2871e95e628013f44
Yash <yashg120602@gmail.com>
2024-04-10 18:02:55 +0530

    local code pushed to github

---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root')); 
+//1.So, in summary, the line you provided is rendering the App component into the HTML element with the ID 'root'
